b3ffc573e9eb77e3a6dd48dc738c38c88ab9c38c
GreeZZly <greezzly7@gmail.com>
2013-12-06 12:02:00 +0400

    registr fix

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -1,4 +1,4 @@
-﻿<link rel="stylesheet" href="/include/styles/welcome.css" type="text/css" media="screen"/>
+<link rel="stylesheet" href="/include/styles/welcome.css" type="text/css" media="screen"/>
 <div id='register_form' class='hidd4en'>
 	<form method='post' action=''>
 		<div class='block logoplace'>

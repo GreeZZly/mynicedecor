26e7fe19af635bf9cd5cf4cc7280e720e7ed5738
GreeZZly <greezzly7@gmail.com>
2013-12-10 17:03:24 +0400

    header adaptive try again and again

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -35,7 +35,7 @@
 		<div id="header_right">
 			<div id="header_right_top">
 				<?if($log_on == 0) { print('<a href="/auth/login"><div class="header_menu_item" id="log_in">Вход</div></a>');} else {print('<a href="/auth/logout"><div class="header_menu_item" id="log_in">Выход</div></a>');}?>
-				<?if($log_on == 0) { print('<a href="/auth/registr"><div class="header_menu_item" id="registr">Зарегистрируйтесь :-)</div></a>');} else {print('<div class="header_menu_item" id="registr"></div>');}?>
+				<?if($log_on == 0) { print('<a href="/auth/registr"><div class="header_menu_item" id="registr">Зарегистрируйтесь</div></a>');} else {print('<div class="header_menu_item" id="registr"></div>');}?>
 				<!-- <a href="/auth/login"><div class="header_menu_item" id="log_in"></div></a> -->
 			</div>
 			<div id="soc_icons">

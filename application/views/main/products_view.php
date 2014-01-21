57ee3f294143751e4efef028db2c15954ca6456f
Egor Kosolapov <greezzly7@gmail.com>
2014-01-21 19:13:09 +0400

    small pics

--- application/views/main/products_view.php
+++ application/views/main/products_view.php
@@ -17,7 +17,10 @@
 					$value['price'] = 'Уточните цену'; 
 					$value['currency'] = '';					
 				}
-				print ('<div class="product_wrapper"><form name="prod_to_cart" method="post" action="/main/insert_to_cart"><a href="/index.php/main/viewProduct/'.$value['id'].'"><div class="pr_img" srv_id="'.$value['id'].'"><img src="http://goodcrm.ru/'.$value['img'].'"></div></a><a href="/index.php/main/viewProduct/'.$value['id'].'"><div class="pr_name" srv_id="'.$value['id'].'">'.$value['name'].'</div></a><div class="pr_type">'.$value['type'].'</div><div class="price">'.$value['price'].' '.$value['currency'].'</div><input type="hidden" name="product_id" value="'.$value['id'].'"><div class="buy_button" srv_id="'.$value['id'].'">Купить</div><div class="like_button" srv_id="'.$value['id'].'" like="dislike"></div></form></div>');
+
+				$position =strlen('/include/images/products/2/')-1;
+				$value['img'] =strpos($value['img'], '/2/')?substr_replace($value['img'], '/small',$position , 0):$value['img']; 
+				print ('<div class="product_wrapper"><form name="prod_to_cart" method="post" action="/main/insert_to_cart"><a href="/index.php/main/viewProduct/'.$value['id'].'"><div class="pr_img" srv_id="'.$value['id'].'"><img src="http://goodcrm.ru'.$value['img'].'"></div></a><a href="/index.php/main/viewProduct/'.$value['id'].'"><div class="pr_name" srv_id="'.$value['id'].'">'.$value['name'].'</div></a><div class="pr_type">'.$value['type'].'</div><div class="price">'.$value['price'].' '.$value['currency'].'</div><input type="hidden" name="product_id" value="'.$value['id'].'"><div class="buy_button" srv_id="'.$value['id'].'">Купить</div><div class="like_button" srv_id="'.$value['id'].'" like="dislike"></div></form></div>');
 			}
 		?>
 	</div>

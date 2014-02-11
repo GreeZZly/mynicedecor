881254fa451ea1973fbb820473598d74ec61d3af
Egor Kosolapov <greezzly7@gmail.com>
2014-02-11 15:42:42 +0400

    sherlock

--- application/views/main/gallery.php
+++ application/views/main/gallery.php
@@ -1,12 +1,12 @@
 <div id="gallery">
 			<div class="large">
-				<img src="/include/images/big/image01.jpg" title="" alt="image01.jpg" />
+				<img src="/include/images/big/bg-sherlock.jpg" title="" alt="bg-sherlock.jpg" />
 				<img style="display:none;" src="/include/images/big/image02.jpg" title="" alt="image01.jpg" />
 				
 			</div>
 		
 			<div id="thumbnail">
-				<div id="yellow" href="/include/images/big/image01.jpg"><div class="arrows yellow_ar hidden"></div><span class="tmb_text">Самые добрые диваны в Е классе</span></div>
+				<div id="yellow" href="/include/images/big/bg-sherlock.jpg"><div class="arrows yellow_ar"></div><span class="tmb_text">Обои от Шерлока</span></div>
 				<div id="green" href="/include/images/big/image02.jpg"><div class="arrows green_ar hidden"></div><span class="tmb_text">Экстрасенсорный дизайн правит балом</span></div>
 				<div id="tomato"  href="/include/images/big/image03.jpg"><div class="arrows tomato_ar hidden"></div><span class="tmb_text">Просто очень красивый дизайн</span></div>
 				<div id="red" href="/include/images/big/image04.jpg"><div class="arrows red_ar hidden"></div><span class="tmb_text">Теплые стены согреют дом</span></div>

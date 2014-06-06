0abfe9e277f4cbf3c0f8ec724560a9de8ef9f156
Egor Kosolapov <greezzly7@gmail.com>
2014-06-06 17:21:31 +0400

    pic

--- application/views/main/gallery.php
+++ application/views/main/gallery.php
@@ -1,12 +1,12 @@
 <div id="gallery">
 			<div class="large">
-				<img src="/include/images/big/image01.jpg" title="" alt="image01.jpg" />
+				<img src="/include/images/big/eklass.jpg" title="" alt="eklass.jpg" />
 				<img style="display:none;" src="/include/images/big/image02.jpg" title="" alt="image01.jpg" />
 				
 			</div>
 		
 			<div id="thumbnail">
-				<div id="yellow" href="/include/images/big/image01.jpg"><div class="arrows yellow_ar"></div><span class="tmb_text">Лучшие диваны в Е-классе</span></div>
+				<div id="yellow" href="/include/images/big/eklass.jpg"><div class="arrows yellow_ar"></div><span class="tmb_text">Лучшие диваны в Е-классе</span></div>
 				<div id="green" href="/include/images/big/image02.jpg"><div class="arrows green_ar hidden"></div><span class="tmb_text">Экстрасенсорный дизайн правит балом</span></div>
 				<div id="tomato"  href="/include/images/big/image03.jpg"><div class="arrows tomato_ar hidden"></div><span class="tmb_text">Просто очень красивый дизайн</span></div>
 				<div id="red" href="/include/images/big/Banner_leto2.jpg"><div class="arrows red_ar hidden"></div><span class="tmb_text">Теплые стены согреют дом</span></div>
